24866b8df1fe82b4f07ba4d703cba73bc3313d06
motorbreath13eh <120326142+motorbreath13eh@users.noreply.github.com>
2023-01-22 11:43:38 +0200

    Update Ergasia_Vres_ton_arithmo_1.2_for.cpp

--- Ergasia_Vres_ton_arithmo_1.2_for.cpp
+++ Ergasia_Vres_ton_arithmo_1.2_for.cpp
@@ -12,26 +12,26 @@ int main()
 	unsigned tyxaios_arithmos, unsigned_arithmou_paixti;
 	float arithmos_paixti;
 	
-	printf("Καλως ήρθατε στο παιχνίδι \"Βρες τον αριθμό\"!!!\n");
-	printf("Το πρόγραμμα βάζει έναν τυχαίο θετικό ακέραιο αριθμό από το 1 έως το 1000!\n");
-	printf("Παρακαλώ προσπαθείστε να ανακαλύψετε ποιος είναι ο τυχαίος αριθμός!!!\n");
-	printf("Έχετε 10 προσπάθειες!\n\n");
+	printf("Welcome to the game \"Guess the number\"!!!\n");
+	printf("The program randomly chooses a positive integer from 1 to 1000!\n");
+	printf("Please try to guess who is the chosen number!!!\n");
+	printf("You have 10 tries!\n\n");
 	
 	srand(time(NULL));
 	tyxaios_arithmos = 1 + rand()%1000;
 	//printf("\n\ntyxaios_arithmos = %d\n\n", tyxaios_arithmos);
 	
-	printf("Παρακαλώ πληκτρολογείστε ένα θετικό ακέραιο αριθμό από το 1 έως το 1000!!\n");
+	printf("Please enter a positive integer from 1 to 1000!!\n");
 	
 	for(int i=1; i<=10; i++)
 	{
-		printf("Προσπάθεια %dη: ", i);
+		printf("%d) ", i);
 		scanf("%f", &arithmos_paixti);
 		unsigned_arithmou_paixti = (unsigned)arithmos_paixti;
 		
 		if(unsigned_arithmou_paixti != arithmos_paixti || unsigned_arithmou_paixti<1 || unsigned_arithmou_paixti>1000)
 		{
-			printf("\a\nΔε δώσατε θετικό ακέραιο αριθμό από το 1 έως το 1000!!!\n");
+			printf("\a\nYou didn't enter a positive integer from 1 to 1000!!!\n");
 			i--;
 		}
 		else if (arithmos_paixti==unsigned_arithmou_paixti && unsigned_arithmou_paixti>=1 && unsigned_arithmou_paixti<=1000)
@@ -46,14 +46,14 @@ int main()
 			}
 			else if(unsigned_arithmou_paixti==tyxaios_arithmos)
 			{
-				printf("Μπράβο\n");
+				printf("Congratulations!! You found it!!!\n");
 				i = 10;
 				flag = true;
 			}
 		}
 		else
 		{
-			printf("\a\nΔε δώσατε θετικό ακέραιο αριθμό από το 1 έως το 1000!!!\n");	
+			printf("\a\nYou didn't enter a positive number from 1 to 1000!!!\n");	
 		}	
 	}
 	
@@ -64,11 +64,11 @@ int main()
 	
 	else if (flag == false)
 	{
-		printf("\nΔυστυχώς δε βρήκατε τον αριθμό.. :(\n");
+		printf("\nUnfortunately, you didn't find the number.. :(\n");
 		sleep(2);
 	}
 	
-	printf("\nΤέλος παιχνιδιού!!\nΣας περιμένουμε ξανά!!!\n\n");
+	printf("\nGame over!!\nPlease, play again in the future!!!\n\n");
 	
 	system("pause");
 	return 0;
